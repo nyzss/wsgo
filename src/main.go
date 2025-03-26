14ac828510b09cbf33de691815079b1d19f8fc19
nyzs <oknakoca@gmail.com>
2025-03-26 10:29:17 +0100

    chore: added logging

--- src/main.go
+++ src/main.go
@@ -2,42 +2,50 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
+	"os"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
-var _ = http.MethodGet
+func initLogger() {
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+
+}
 
 func main() {
+	initLogger()
+
 	port := ":8080"
-	fmt.Println("Hello, World!")
 
 	http.HandleFunc("/", handleConnection)
-	fmt.Printf("success: Serving on localhost%s\n", port)
+	log.Info().Str("port", port).Msg("Serving on localhost")
 	err := http.ListenAndServe(port, nil)
 	if err != nil {
-		log.Fatal("fatal: couldn't listen and serve, current port: ", port)
+		log.Fatal().Str("port", port).Msg("couldn't listen and serve, current port")
 	}
 }
 
 func handleConnection(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("REQUEST: ", r)
-
-	fmt.Println("---------------")
+	log.Info().Msg("==== REQUEST DETAILS START ====")
+	log.Info().Interface("request", r).Msg("REQUEST")
+	log.Info().Msg("---------------")
 
 	connection := r.Header.Get("Connection")
 	upgrade := r.Header.Get("Upgrade")
 	key := r.Header.Get("Sec-WebSocket-Key")
 	version := r.Header.Get("Sec-WebSocket-Version")
 
-	fmt.Println("CONNECTION: ", connection)
-	fmt.Println("UPGRADE: ", upgrade)
-	fmt.Println("WEBSOCKET_KEY", key)
-	fmt.Println("WEBSOCKET_VERSION", version)
+	log.Info().Str("value", connection).Msg("CONNECTION")
+	log.Info().Str("value", upgrade).Msg("UPGRADE")
+	log.Info().Str("value", key).Msg("WEBSOCKET_KEY")
+	log.Info().Str("value", version).Msg("WEBSOCKET_VERSION")
+	log.Info().Msg("==== REQUEST DETAILS END ====")
 
 	// todo: setting as true for now to test hijacking, remove later on
 	if true || connection == "Upgrade" && upgrade == "websocket" {
-		fmt.Println("success: WEBSOCKET CONNECTION ASKED")
 		hj, ok := w.(http.Hijacker)
 		if !ok {
 			http.Error(w, "fatal: websocket doesn't support hijacking", http.StatusInternalServerError)
@@ -52,21 +60,30 @@ func handleConnection(w http.ResponseWriter, r *http.Request) {
 		}
 
 		defer conn.Close()
-		bufrw.WriteString("writing from hijacked http server, bbb123")
+		log.Info().Msg("client connected successfully")
+		log.Info().Str("address", conn.RemoteAddr().String()).Msg("client address")
+		bufrw.WriteString("writing from hijacked http server, bbb123\n")
 		bufrw.Flush()
 
 		s, err := bufrw.ReadString('\n')
 
 		if err != nil {
-			log.Printf("fatal: error reading string: %v", err)
+			log.Error().Err(err).Msg("error reading string")
 			return
 		}
 		fmt.Fprintf(bufrw, "You said: %q\nBye.\n", s)
 		bufrw.Flush()
 	} else {
-		fmt.Println("warning: normal http request")
+		log.Warn().Msg("normal http request")
 		// todo: remove after testing hijacking
-		log.Fatal("testing hijacking: you shouldn't see this ")
+		log.Fatal().Msg("testing hijacking")
 	}
-
 }
+
+/*
+GET / HTTP/1.1
+Host: localhost:8080
+Connection: upgrade
+Upgrade: websocket
+
+*/
